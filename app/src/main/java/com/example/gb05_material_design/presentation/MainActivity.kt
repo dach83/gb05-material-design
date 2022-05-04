bbb4f90fad33e610058475d102587ca987643afc
dach83 <dach83@mail.ru>
2022-05-04 09:19:53 +0300

    added fragment PictureOfTheDayFragment

---
+++ app/src/main/java/com/example/gb05_material_design/presentation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gb05_material_design.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gb05_material_design.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
