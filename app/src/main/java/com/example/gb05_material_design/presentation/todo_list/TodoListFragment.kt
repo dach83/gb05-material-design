4570d09f937831d8f950098ba4e423089c9b046e
dach83 <dach83@mail.ru>
2022-05-27 14:47:30 +0300

    Created empty fragment for todo list

---
+++ app/src/main/java/com/example/gb05_material_design/presentation/todo_list/TodoListFragment.kt
@@ -0,0 +1,8 @@
+package com.example.gb05_material_design.presentation.todo_list
+
+import androidx.fragment.app.Fragment
+import com.example.gb05_material_design.R
+
+class TodoListFragment : Fragment(R.layout.fragment_todo_list) {
+
+}
