f9080e324a2fd344cd03e3c01e163fd6012aea3f
dach83 <dach83@mail.ru>
2022-05-29 10:43:06 +0300

    Show mixed content (text and images) in recycler view

---
+++ app/src/main/java/com/example/gb05_material_design/presentation/todo_list/TodoListAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.gb05_material_design.presentation.todo_list
+
+import coil.load
+import com.example.gb05_material_design.databinding.ItemImageTodoBinding
+import com.example.gb05_material_design.databinding.ItemTextTodoBinding
+import com.example.gb05_material_design.domain.model.todo_list.TodoImageItem
+import com.example.gb05_material_design.domain.model.todo_list.TodoItem
+import com.example.gb05_material_design.domain.model.todo_list.TodoTextItem
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
+
+fun todoTextAdapterDelegate(onClickItem: (TodoTextItem) -> Unit) = adapterDelegateViewBinding<TodoTextItem, TodoItem, ItemTextTodoBinding>(
+    { layoutInflater, root -> ItemTextTodoBinding.inflate(layoutInflater, root, false) }
+) {
+    binding.root.setOnClickListener { onClickItem(item) }
+    bind {
+        binding.todoTextView.text = item.text
+    }
+}
+
+fun todoImageAdapterDelegate(onClickItem: (TodoImageItem) -> Unit) = adapterDelegateViewBinding<TodoImageItem, TodoItem, ItemImageTodoBinding>(
+    { layoutInflater, root -> ItemImageTodoBinding.inflate(layoutInflater, root, false) }
+) {
+    binding.root.setOnClickListener { onClickItem(item) }
+    bind {
+        binding.todoImageView.load(item.url)
+    }
+}
--- app/src/main/java/com/example/gb05_material_design/presentation/todo_list/TodoListFragment.kt
+++ app/src/main/java/com/example/gb05_material_design/presentation/todo_list/TodoListFragment.kt
@@ -1,8 +1,43 @@
 package com.example.gb05_material_design.presentation.todo_list
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.gb05_material_design.R
+import com.example.gb05_material_design.databinding.FragmentTodoListBinding
+import com.example.gb05_material_design.domain.model.todo_list.TodoImageItem
+import com.example.gb05_material_design.domain.model.todo_list.TodoItem
+import com.example.gb05_material_design.domain.model.todo_list.TodoTextItem
+import com.example.gb05_material_design.domain.model.todo_list.randomTodoList
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 
 class TodoListFragment : Fragment(R.layout.fragment_todo_list) {
+
+    private val binding by viewBinding(FragmentTodoListBinding::bind)
+
+    private val adapter = ListDelegationAdapter<List<TodoItem>>(
+        todoTextAdapterDelegate(::onClickTodoTextItem),
+        todoImageAdapterDelegate(::onClickTodoImageItem),
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        adapter.items = randomTodoList(30)
+        binding.todoRecyclerView.adapter = adapter
+    }
+
+    private fun onClickTodoTextItem(item: TodoTextItem) {
+        Toast.makeText(context, "Click text", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onClickTodoImageItem(item: TodoImageItem) {
+        Toast.makeText(context, "Click image", Toast.LENGTH_SHORT).show()
+    }
 
 }
